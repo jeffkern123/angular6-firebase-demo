c66a0e374ac0102be9e6daaa27434624bc70d92f
jkern <jkern@ORLDITMG18>
2018-07-09 08:56:46 -0400

    Format app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
+  declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
