732cd05d0c4197024a80e1139034765c8b6f97aa
jkern <jkern@ORLDITMG18>
2018-07-09 10:30:09 -0400

    import {MatButtonModule}

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, MatButtonModule],
   declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
