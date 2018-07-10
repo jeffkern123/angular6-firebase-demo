c1202dd8fbad40c677bbc319d42e4908d5c4d021
jkern <jkern@ORLDITMG18>
2018-07-10 15:35:31 -0400

    ng g component components/restaurants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material';
+import { MatButtonModule } from '@angular/material';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
+import { RestaurantsComponent } from './components/restaurants/restaurants.component';
 
 @NgModule({
-  imports: [BrowserModule, MatButtonModule],
-  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    MatButtonModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
+  ],
+  declarations: [AppComponent, RestaurantsComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
