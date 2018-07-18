d71e92411d41535fa978c766419213c17c2efa86
jkern <jkern@ORLDITMG18>
2018-07-18 13:08:35 -0400

    Added firebase init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material';
+
 import { AngularFireModule } from 'angularfire2';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
+// import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
@@ -13,10 +14,9 @@ import { RestaurantService } from './service/restaurant.service';
 @NgModule({
   imports: [
     BrowserModule,
-    MatButtonModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFirestoreModule
+    // AngularFirestoreModule
   ],
   declarations: [AppComponent, RestaurantsComponent],
   providers: [RestaurantService],
